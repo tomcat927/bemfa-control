4a9a92f42e0ee5e160b09ab5ae2a92d9a40d0eec
tomcat927 <63151253+tomcat927@users.noreply.github.com>
2026-09-08 14:30:41 +0800

    fix: register Application class

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tomcat927.bemfacontrol"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
