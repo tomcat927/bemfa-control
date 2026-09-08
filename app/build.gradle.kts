725d0137de67f7311f54a3069295feca2378b366
tom <tomcat1258@163.com>
2026-09-08 19:55:43 +0800

    fix: remove duplicate activity tag in AndroidManifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.tomcat927.bemfacontrol"
         minSdk = 26
         targetSdk = 34
-        versionCode = 12
-        versionName = "2026.09.08.2100"
+        versionCode = 13
+        versionName = "2026.09.08.2120"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
